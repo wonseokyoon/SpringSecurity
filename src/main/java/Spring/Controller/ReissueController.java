344c4cc508f26017c953848e24612f272d103256
wonseokyoon <01088115030a@gmail.com>
2025-02-12 17:09:48 +0900

    feature Reissue token

--- src/main/java/Spring/Controller/ReissueController.java
+++ src/main/java/Spring/Controller/ReissueController.java
@@ -1,72 +1,74 @@
-//package Spring.Controller;
-//
-//import Spring.Exception.BaseException;
-//import Spring.Exception.ErrorCode;
-//import Spring.Jwt.JWTUtil;
-//import io.jsonwebtoken.ExpiredJwtException;
-//import jakarta.servlet.http.Cookie;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.transaction.annotation.Transactional;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequiredArgsConstructor
-//public class ReissueController {
-//    private final JWTUtil jwtUtil;
-//
-//    @PostMapping("/reissue")
-//    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) throws BaseException {
-//
-//        //refresh 토큰
-//        String refresh = null;
-//        Cookie[] cookies = request.getCookies();
-//        for (Cookie cookie : cookies) {
-//
-//            if (cookie.getName().equals("refresh")) {
-//
-//                refresh = cookie.getValue();
-//            }
-//        }
-//
-//        if (refresh == null) {
-//            //response status code
-//            throw new BaseException(ErrorCode.REFRESH_TOKEN_NULL);
-//        }
-//
-//        //만료 체크
-//        try {
-//            jwtUtil.isExpired(refresh);
-//        } catch (ExpiredJwtException e) {
-//            throw new BaseException(ErrorCode.TOKEN_IS_EXPIRED);
-//        }
-//
-//        // 토큰이 refresh인지 확인 (발급시 페이로드에 명시)
-//        String category = jwtUtil.getCategory(refresh);
-//
-//        if (!category.equals("refresh")) {
-//
-//            //response status code
-//            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
-//        }
-//
-//        String username = jwtUtil.getUsername(refresh);
-//        String role = jwtUtil.getRole(refresh);
-//
-//        //make new JWT
-//        String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
-//
-//        //response
+package Spring.Controller;
+
+import Spring.Exception.BaseException;
+import Spring.Exception.ErrorCode;
+import Spring.Jwt.JWTUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class ReissueController {
+    // refresh 토큰으로 access 토큰 재발급
+
+    private final JWTUtil jwtUtil;
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(HttpServletRequest request) throws BaseException {
+
+        //refresh 토큰
+        String refresh = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("refresh")) {
+                refresh = cookie.getValue();
+                break;
+            }
+        }
+
+        if (refresh == null) {
+            //response status code
+            throw new BaseException(ErrorCode.REFRESH_TOKEN_NULL);
+        }
+
+        //만료 체크
+        try {
+            jwtUtil.isExpired(refresh);
+        } catch (ExpiredJwtException e) {
+            throw new BaseException(ErrorCode.TOKEN_IS_EXPIRED);
+        }
+
+        // 토큰이 refresh인지 확인 (발급시 페이로드에 명시)
+        String category = jwtUtil.getCategory(refresh);
+
+        if (!category.equals("refresh")) {
+            //response status code
+            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
+        }
+
+        String username = jwtUtil.getUsername(refresh);
+        String role = jwtUtil.getRole(refresh);
+
+        //make new JWT
+        String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
+
+        //response
+        Map<String,String> response=new HashMap<>();
+        response.put("new_accessToken",newAccess);
 //        response.setHeader("access", newAccess);
-//
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
-//
-//
-//
-//}
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+
+
+}
